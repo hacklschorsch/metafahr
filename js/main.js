8a04e0dae7004034d79bb05949afc59a81d3e5a9
Florian Sesser <florian@sesser.at>
2013-07-15 01:37:12 +0200

    somemore

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
-YUI().use('node', 'transition', 'resize', function (Y) {
+YUI().use('node', 'transition', function (Y) {
 
     Y.one('#findRides').on('click', function () {
         var fromCity = Y.one('#fromCity').get('value');
         var toCity = Y.one('#toCity').get('value');
 
-        Y.one('#container').transition({
+        Y.one('#query').transition({
             duration: 0.5, // secs
             'margin-top': '2em'
-        });
+        }, function () {alert('boom!');});
 
         var mfgUrl = 'http://www.mitfahrgelegenheit.de/mitfahrzentrale/' +
                 fromCity + '/' + toCity + '.html';
         Y.one('#results').empty().append('<iframe src="' + mfgUrl + '" class="result" />');
-        var mfgResize = new Y.Resize({node: '.result'});
     });
 
 });
