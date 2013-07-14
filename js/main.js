e17fcc07d26e01fa448e9e3b262f31c57110d3e4
Florian Sesser <florian@sesser.at>
2013-07-15 01:05:02 +0200

    some more code.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-YUI().use('node', function (Y) {
+YUI().use('node', 'transition', 'resize', function (Y) {
+
     Y.one('#findRides').on('click', function () {
-        alert("!!!");
+        Y.one('#container').transition({
+            duration: 0.5, // secs
+            'margin-top': '2em'
+        });
+
+        var mfgUrl = 'http://www.mitfahrgelegenheit.de/mitfahrzentrale/M%C3%BCnchen/Erlangen.html';
+        Y.one('#results').empty().append('<iframe src="' + mfgUrl + '" class="result" />');
+        var mfgResize = new Y.Resize({node: '.result'});
     });
+
 });
+
