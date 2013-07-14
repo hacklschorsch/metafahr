70ac8178e2f8e2504b1f554150fd1b6cceb6b3f0
Florian Sesser <florian@sesser.at>
2013-07-15 01:11:36 +0200

    Make it work lol

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 YUI().use('node', 'transition', 'resize', function (Y) {
 
     Y.one('#findRides').on('click', function () {
+        var fromCity = Y.one('#fromCity').get('value');
+        var toCity = Y.one('#toCity').get('value');
+
         Y.one('#container').transition({
             duration: 0.5, // secs
             'margin-top': '2em'
         });
 
-        var mfgUrl = 'http://www.mitfahrgelegenheit.de/mitfahrzentrale/M%C3%BCnchen/Erlangen.html';
+        var mfgUrl = 'http://www.mitfahrgelegenheit.de/mitfahrzentrale/' +
+                fromCity + '/' + toCity + '.html';
         Y.one('#results').empty().append('<iframe src="' + mfgUrl + '" class="result" />');
         var mfgResize = new Y.Resize({node: '.result'});
     });
