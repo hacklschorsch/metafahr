6205e3787e2b191d6f6739b4b4b6a141a59ca7ef
Florian Sesser <florian@sesser.at>
2014-12-10 06:13:14 +0100

    don't upload .swp files (jeeez, where have i left my dotfiles)

--- publish.sh
+++ publish.sh
@@ -2,4 +2,4 @@
 
 # rsync to web server.
 
-rsync -rv -e 'ssh -p 2020' --exclude '.git*' . fsesser@c2.cosetrain.com:metafahr-website.lnk/
+rsync -rv -e 'ssh -p 2020' --exclude '.git*' --exclude '.*.swp' . fsesser@c2.cosetrain.com:metafahr-website.lnk/
